8d095947a39fcd4b0936ee35366c107565460231
Kratik Nayak <knayak@ganakalabs.com>
2020-07-10 01:21:47 +0530

    Minor responsive fixes

--- src/views/article.jsx
+++ src/views/article.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { PageHeader, Button, Tag, Typography } from "antd";
+import { PageHeader, Button, Row, Col, Typography } from "antd";
 const { Paragraph } = Typography;
 
 const Article = (props) => {
   console.log(props.location.state);
 
   const content = (
-    <div className="text-center">
+    <div className="text-center individual-article">
       <img
         src={props.location.state.urlToImage}
         alt="img"
-        className="w-90"
+        className="w-100"
       ></img>
-      <Paragraph className="font-weight-bold m-3  bg-transparent">
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} className="text-left">
+          Source - {props.location.state.sources}
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={12} xl={12} className="text-right">
+          {new Date(props.location.state.publishedAt).toDateString()}
+        </Col>
+      </Row>
+      <Paragraph className="font-weight-bold  bg-transparent">
         {props.location.state.description}
       </Paragraph>
-      <Paragraph className="font-weight-bold m-3 bg-transparent">
+      <Paragraph className="font-weight-bold bg-transparent">
         {props.location.state.content}
       </Paragraph>
     </div>
@@ -24,14 +32,7 @@ const Article = (props) => {
     <>
       <PageHeader
         title={props.location.state.title}
-        className="site-page-header mx-5"
-        subTitle={props.location.state.sources}
-        tags={
-          <Tag>
-            Published On -{" "}
-            {new Date(props.location.state.publishedAt).toDateString()}
-          </Tag>
-        }
+        className="individual-article-site-page-header"
         extra={[
           <Button
             key="3"
