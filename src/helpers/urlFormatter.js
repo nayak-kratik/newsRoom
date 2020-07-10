09eb07162098f780efec8aef9cef95f2f835ba35
Kratik Nayak <knayak@ganakalabs.com>
2020-07-10 19:55:33 +0530

    only build folder

--- src/helpers/urlFormatter.js
+++
@@ -1,8 +0,0 @@
-export function formatUrl(source) {
-  const formattedUrl = [];
-  for (let d in source)
-    formattedUrl.push(
-      encodeURIComponent(d) + "=" + encodeURIComponent(source[d])
-    );
-  return formattedUrl.join("&");
-}
--- src/utils/colorPalette.js
+++
@@ -1,5 +0,0 @@
-export const COLOR1 = "#f7f7f7";
-export const COLOR2 = "#efefef";
-export const COLOR3 = "#e4e4e4";
-export const COLOR4 = "#d8d6d6";
-export const COLOR5 = "#cac8c8";
