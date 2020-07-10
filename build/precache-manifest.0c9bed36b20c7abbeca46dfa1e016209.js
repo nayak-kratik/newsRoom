ab3ac4b4301ea49e0686d2b936b2eb1948db7f81
Kratik Nayak <knayak@ganakalabs.com>
2020-07-10 19:44:39 +0530

    build folder added

---
+++ build/precache-manifest.0c9bed36b20c7abbeca46dfa1e016209.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc320e92ab4a913c5c8f55b1552dee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab7a59cff69718b396c",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "f9c96b053c452a18acd4",
+    "url": "/static/css/main.a2494392.chunk.css"
+  },
+  {
+    "revision": "8ab7a59cff69718b396c",
+    "url": "/static/js/2.e997736e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e997736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c96b053c452a18acd4",
+    "url": "/static/js/main.e981cdba.chunk.js"
+  },
+  {
+    "revision": "b042b6c612a9e58fa29a",
+    "url": "/static/js/runtime-main.cbb54062.js"
+  },
+  {
+    "revision": "62c98adccdb25f45c600727781460afd",
+    "url": "/static/media/proximanova-black-webfont.62c98adc.svg"
+  },
+  {
+    "revision": "7faec3de6e1a93afc247ecaa77ef7c21",
+    "url": "/static/media/proximanova-black-webfont.7faec3de.woff"
+  },
+  {
+    "revision": "c8268715c59f0916c18f6ecbbfd658ba",
+    "url": "/static/media/proximanova-black-webfont.c8268715.woff2"
+  },
+  {
+    "revision": "d34372fbc199829a89ffdb28157b4b94",
+    "url": "/static/media/proximanova-black-webfont.d34372fb.eot"
+  },
+  {
+    "revision": "ea287230ab5db18f8b8d5e0f53db8319",
+    "url": "/static/media/proximanova-black-webfont.ea287230.ttf"
+  },
+  {
+    "revision": "2ae75b960ca2cebd46049b8547124e46",
+    "url": "/static/media/proximanova-bold-webfont.2ae75b96.woff"
+  },
+  {
+    "revision": "5dd086df8b1012a37b492e81c351f526",
+    "url": "/static/media/proximanova-bold-webfont.5dd086df.svg"
+  },
+  {
+    "revision": "93c4dd9a7259bef4e32c00b9c8af5cc5",
+    "url": "/static/media/proximanova-bold-webfont.93c4dd9a.ttf"
+  },
+  {
+    "revision": "d3fbf97b59de86d2a515e4befc859d92",
+    "url": "/static/media/proximanova-bold-webfont.d3fbf97b.woff2"
+  },
+  {
+    "revision": "fce0c035c3f9151c6ce47873c5911935",
+    "url": "/static/media/proximanova-bold-webfont.fce0c035.eot"
+  },
+  {
+    "revision": "34355efb1077e829412356bc61a2872c",
+    "url": "/static/media/proximanova-extrabold-webfont.34355efb.woff2"
+  },
+  {
+    "revision": "6509b41f870f8a9a0c2060c5e07e8c69",
+    "url": "/static/media/proximanova-extrabold-webfont.6509b41f.svg"
+  },
+  {
+    "revision": "7770dc45dcbe42ea65901ffefb903d60",
+    "url": "/static/media/proximanova-extrabold-webfont.7770dc45.eot"
+  },
+  {
+    "revision": "a20738642cf43c4e77242f4ba3e3a437",
+    "url": "/static/media/proximanova-extrabold-webfont.a2073864.woff"
+  },
+  {
+    "revision": "ca0833a207b1fd33791a4e6c25cbaa79",
+    "url": "/static/media/proximanova-extrabold-webfont.ca0833a2.ttf"
+  },
+  {
+    "revision": "171a7ed33666ab774e76a16008d45653",
+    "url": "/static/media/proximanova-light-webfont.171a7ed3.woff2"
+  },
+  {
+    "revision": "89465e4bcbc290f41f8f33caf271a474",
+    "url": "/static/media/proximanova-light-webfont.89465e4b.eot"
+  },
+  {
+    "revision": "8caf62e4806cede912b94ff972a76678",
+    "url": "/static/media/proximanova-light-webfont.8caf62e4.ttf"
+  },
+  {
+    "revision": "d45c1c1f16caed5e30484909f360df4e",
+    "url": "/static/media/proximanova-light-webfont.d45c1c1f.woff"
+  },
+  {
+    "revision": "d67dd6e4e32523de38adf7c5edb96366",
+    "url": "/static/media/proximanova-light-webfont.d67dd6e4.svg"
+  },
+  {
+    "revision": "326c618bdaf5188c69cd651148abd054",
+    "url": "/static/media/proximanova-regular-webfont.326c618b.woff"
+  },
+  {
+    "revision": "480e6e79d1b76b469d4e37bb54ec6deb",
+    "url": "/static/media/proximanova-regular-webfont.480e6e79.eot"
+  },
+  {
+    "revision": "9c84d677515460975500a2d3d9f2398a",
+    "url": "/static/media/proximanova-regular-webfont.9c84d677.ttf"
+  },
+  {
+    "revision": "e107b29e2c09d137720b241b304de7f8",
+    "url": "/static/media/proximanova-regular-webfont.e107b29e.svg"
+  },
+  {
+    "revision": "ef7e71575e6464317a9692fd61f2bf42",
+    "url": "/static/media/proximanova-regular-webfont.ef7e7157.woff2"
+  },
+  {
+    "revision": "23ebe2926328b001b4fb565d7b2e99d2",
+    "url": "/static/media/proximanova-semibold-webfont.23ebe292.svg"
+  },
+  {
+    "revision": "3b9807d1adbfad9d90141bef32822c76",
+    "url": "/static/media/proximanova-semibold-webfont.3b9807d1.eot"
+  },
+  {
+    "revision": "bc0644655a64313cd672aaac7c5c30e2",
+    "url": "/static/media/proximanova-semibold-webfont.bc064465.ttf"
+  },
+  {
+    "revision": "d113a060af8d07a15449d4125e0bc013",
+    "url": "/static/media/proximanova-semibold-webfont.d113a060.woff"
+  }
+]);
